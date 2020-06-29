88dccf56f7632b2146846279bba0e07b4617959e
Kayla Kuhlman <kakoreo4@gmail.com>
2020-06-28 22:00:51 -0500

    first commit

---
+++ src/main/java/com/blissfullycoding/MarsRoverAppApplication.java
@@ -0,0 +1,13 @@
+package com.blissfullycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverAppApplication.class, args);
+	}
+
+}
