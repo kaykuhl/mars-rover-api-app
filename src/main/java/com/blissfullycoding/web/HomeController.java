672e1d7e5252bf3f68de3b26b30e45dcb5b54b4b
Kayla Kuhlman <kakoreo4@gmail.com>
2020-07-12 15:08:08 -0500

    created necessary classes and now ready for front end development

--- src/main/java/com/blissfullycoding/web/HomeController.java
+++ src/main/java/com/blissfullycoding/web/HomeController.java
@@ -1,6 +1,7 @@
-package com.blissfullycoding;
+package com.blissfullycoding.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String getHomeView () {
+	public String getHomeView (ModelMap model) {
+
 		return "index";
 	}
-	
 }
